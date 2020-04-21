ffa92d7a3748df987070fd30bd68394606a7bbea
Ian Pires <ian-pires@hotmail.com>
2020-04-21 20:48:00 -0300

    Merge entre Cadastro-LM e Cadastro-NM

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,32 +2,23 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
-<<<<<<< HEAD
 import PessoaEndereco from './pages/PessoaEndereco';
 import Endereco from './pages/Endereco';
-
-=======
 import CadastroSec from './pages/Cadastro-sec';
 import CadastroTer from './pages/Cadastro-ter';
 import CadastroQuat from './pages/Cadastro-quat';
->>>>>>> origin/Cadastro-NM
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login}></Route>
-<<<<<<< HEAD
-                <Route path="/cadastro" exact component={Cadastro}></Route>
-                <Route path="/pessoa" exact component={PessoaEndereco}></Route>
-                <Route path="/endereco" exact component={Endereco}></Route>
-=======
-                <Route path="/cadastrar" exact component={Cadastro}></Route>
-                <Route path="/informacao" exact component={CadastroSec}></Route>
-                <Route path="/cuidador" exact component={CadastroTer}></Route>
-                <Route path="/termo" exact component={CadastroQuat}></Route>
-                
->>>>>>> origin/Cadastro-NM
+                <Route path="/cadastrar"  component={Cadastro}></Route>
+                <Route path="/informacao"  component={CadastroSec}></Route>
+                <Route path="/cuidador"  component={CadastroTer}></Route>
+                <Route path="/termo"  component={CadastroQuat}></Route>
+                <Route path="/pessoa"  component={PessoaEndereco}></Route>
+                <Route path="/endereco"  component={Endereco}></Route>
             </Switch>
         </BrowserRouter>
     );
