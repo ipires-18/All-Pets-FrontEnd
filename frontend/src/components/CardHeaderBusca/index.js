aece8c5806a27a7850a44cbf62924958699f05aa
Qwert <lehlagoo>
2020-06-03 23:14:16 -0300

    em progresso Header de Busca

---
+++ frontend/src/components/CardHeaderBusca/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const CardHeaderBusca = () =>{
+    return (
+        <>
+        <Container className="container-busca">
+            <Row>
+                <h3 className="title-busca">Busca</h3>
+                <div>
+                    
+                </div>
+            </Row>
+        </Container>
+        </>
+    );
+}
+
+export default CardHeaderBusca;
--- frontend/src/pages/PainelDeControle/PainelDono/PainelResultado/index.js
+++ frontend/src/pages/PainelDeControle/PainelDono/PainelResultado/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavPainel from '../../../../components/NavPainel';
+import CardHeaderBusca from '../../../../components/CardHeaderBusca';
 
 
 const PainelResultado = () => {
   return (
     <>
       <NavPainel/>
-      <h1>Tela de Propostas</h1> 
+      <CardHeaderBusca/>
+      <h1>Tela de Propostas</h1>
     </>
   )
 }
