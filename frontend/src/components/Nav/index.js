824fc41490204c8c089d0fa8f19d0f4bf8f091aa
ipires-18 <ian-pires@hotmail.com>
2020-06-05 22:16:55 -0300

    Cadastro de Login e Cadastro de Informação pessoal

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom';
 import { MDBNav, MDBNavLink } from "mdbreact";
 
-export 
 
 export default () => (
   <BrowserRouter>
