edefd1ca5ec31031b6f08a063ac35ad42a63e5f0
Ian Pires de Oliveira <ian.oliveira@linx.com.br>
2020-06-21 20:42:28 -0300

    nova api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:8080'
+    baseURL:'ec2-54-233-224-134.sa-east-1.compute.amazonaws.com'
 })
 
 export default api;
