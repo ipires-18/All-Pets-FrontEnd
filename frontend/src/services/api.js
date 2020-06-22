5a2545bb1c7dfca9f9512dd2126c872a486a0185
Igor Santos <igor.santos@keycar.com.br>
2020-06-21 21:28:42 -0300

    update api host in services/api.sj

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'ec2-54-233-224-134.sa-east-1.compute.amazonaws.com'
+    baseURL:'http://ec2-54-233-224-134.sa-east-1.compute.amazonaws.com:8080'
 })
 
 export default api;
